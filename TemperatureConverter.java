076f4ad2b725a5469a79aa71a0508db6840bb7f6
Saurabh Mathe <105196153+WhoisSwitchX@users.noreply.github.com>
2024-01-05 17:03:38 +0530

    Update TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
 public class TemperatureConverter {
+    
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Welcome to the Temperature Converter!");
